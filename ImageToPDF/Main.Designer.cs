ef084f0502c07d96e56fa8edbd186f8266044317
aazhbd <aazhbd@yahoo.com>
2019-08-31 10:19:57 +0200

    Adding project for Image to PDF.

---
+++ ImageToPDF/Main.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace ImageToPDF
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sourcePath = new System.Windows.Forms.TextBox();
+            this.sourceButton = new System.Windows.Forms.Button();
+            this.SourceLabel = new System.Windows.Forms.Label();
+            this.DestinationLabel = new System.Windows.Forms.Label();
+            this.DestinationPath = new System.Windows.Forms.TextBox();
+            this.destinationButton = new System.Windows.Forms.Button();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.helpLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // sourcePath
+            // 
+            this.sourcePath.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sourcePath.Location = new System.Drawing.Point(136, 13);
+            this.sourcePath.Name = "sourcePath";
+            this.sourcePath.Size = new System.Drawing.Size(328, 24);
+            this.sourcePath.TabIndex = 0;
+            // 
+            // sourceButton
+            // 
+            this.sourceButton.Location = new System.Drawing.Point(462, 12);
+            this.sourceButton.Name = "sourceButton";
+            this.sourceButton.Size = new System.Drawing.Size(110, 25);
+            this.sourceButton.TabIndex = 1;
+            this.sourceButton.Text = "Open File";
+            this.sourceButton.UseVisualStyleBackColor = true;
+            this.sourceButton.Click += new System.EventHandler(this.SourceButton_Click);
+            // 
+            // SourceLabel
+            // 
+            this.SourceLabel.AutoSize = true;
+            this.SourceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SourceLabel.Location = new System.Drawing.Point(7, 16);
+            this.SourceLabel.Name = "SourceLabel";
+            this.SourceLabel.Size = new System.Drawing.Size(123, 18);
+            this.SourceLabel.TabIndex = 2;
+            this.SourceLabel.Text = "Path to the Image";
+            // 
+            // DestinationLabel
+            // 
+            this.DestinationLabel.AutoSize = true;
+            this.DestinationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DestinationLabel.Location = new System.Drawing.Point(7, 62);
+            this.DestinationLabel.Name = "DestinationLabel";
+            this.DestinationLabel.Size = new System.Drawing.Size(101, 18);
+            this.DestinationLabel.TabIndex = 3;
+            this.DestinationLabel.Text = "Folder to PDF";
+            // 
+            // DestinationPath
+            // 
+            this.DestinationPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DestinationPath.Location = new System.Drawing.Point(136, 60);
+            this.DestinationPath.Name = "DestinationPath";
+            this.DestinationPath.Size = new System.Drawing.Size(328, 24);
+            this.DestinationPath.TabIndex = 4;
+            // 
+            // destinationButton
+            // 
+            this.destinationButton.Location = new System.Drawing.Point(462, 60);
+            this.destinationButton.Name = "destinationButton";
+            this.destinationButton.Size = new System.Drawing.Size(110, 24);
+            this.destinationButton.TabIndex = 5;
+            this.destinationButton.Text = "Open Folder";
+            this.destinationButton.UseVisualStyleBackColor = true;
+            this.destinationButton.Click += new System.EventHandler(this.DestinationButton_Click);
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(362, 118);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(210, 31);
+            this.convertButton.TabIndex = 6;
+            this.convertButton.Text = "Convert To PDF";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(258, 118);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(98, 31);
+            this.resetButton.TabIndex = 7;
+            this.resetButton.Text = "Reset Path";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // helpLinkLabel
+            // 
+            this.helpLinkLabel.AutoSize = true;
+            this.helpLinkLabel.Location = new System.Drawing.Point(12, 127);
+            this.helpLinkLabel.Name = "helpLinkLabel";
+            this.helpLinkLabel.Size = new System.Drawing.Size(137, 13);
+            this.helpLinkLabel.TabIndex = 9;
+            this.helpLinkLabel.TabStop = true;
+            this.helpLinkLabel.Text = "http://articulatedlogic.com/";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 161);
+            this.Controls.Add(this.helpLinkLabel);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.destinationButton);
+            this.Controls.Add(this.DestinationPath);
+            this.Controls.Add(this.DestinationLabel);
+            this.Controls.Add(this.SourceLabel);
+            this.Controls.Add(this.sourceButton);
+            this.Controls.Add(this.sourcePath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "Image To PDF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox sourcePath;
+        private System.Windows.Forms.Button sourceButton;
+        private System.Windows.Forms.Label SourceLabel;
+        private System.Windows.Forms.Label DestinationLabel;
+        private System.Windows.Forms.TextBox DestinationPath;
+        private System.Windows.Forms.Button destinationButton;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.LinkLabel helpLinkLabel;
+    }
+}
+
---
+++ ImageToPDF/Main.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace ImageToPDF
+{
+    public partial class Main : Form
+    {
+        string fname;
+        public Main()
+        {
+            this.fname = "output";
+            InitializeComponent();
+        }
+
+        private void SourceButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "image files (*.jpg)|*.jpg|All files (*.*)|*.*";
+            dialog.Title = "Select an Image to convert to PDF";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                sourcePath.Text = dialog.FileName;
+                DestinationPath.Text = Path.GetDirectoryName(sourcePath.Text);
+                string[] safeNames = (dialog.SafeFileName).Split('.');
+                var names = safeNames.Take<string>(safeNames.Length - 1);
+                this.fname = String.Join(".", names);
+            }
+        }
+
+        private void DestinationButton_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+                    DestinationPath.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            sourcePath.Text = "";
+            DestinationPath.Text = "";
+        }
+
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+            convertButton.Enabled = false;
+            if (sourcePath.Text == "" || DestinationPath.Text == "")
+            {
+                MessageBox.Show("Select paths to create PDF.");
+                convertButton.Enabled = true;
+                return;
+            }
+
+            Document document = new Document();
+            using (var stream = new FileStream(DestinationPath.Text + "\\" + this.fname + ".pdf", FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                PdfWriter.GetInstance(document, stream);
+                document.Open();
+                using (FileStream imageStream = new FileStream(sourcePath.Text, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    iTextSharp.text.Image image = iTextSharp.text.Image.GetInstance(imageStream);
+                    image.SetAbsolutePosition(0, 0);
+                    
+                    if(image.ScaledHeight > document.PageSize.Height)
+                    {
+                        image.ScaleAbsoluteHeight(document.PageSize.Height);
+                    }
+
+                    if(image.ScaledWidth > document.PageSize.Width)
+                    {
+                        image.ScaleAbsoluteWidth(document.PageSize.Width);
+                    }
+
+                    document.Add(image);
+                }
+                document.Close();
+            }
+            convertButton.Enabled = true;
+
+        }
+    }
+}
